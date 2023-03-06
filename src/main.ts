de20f9a549f6c6185ae9b0b95cef8c4e01c9b929
kjkandrea <kjkandrea@gmail.com>
2023-03-06 13:10:14 +0900

    feat: google map 추가

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,18 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
 
-console.log(import.meta.env.VITE_SOME_KEY);
+// @ts-ignore
+let map: google.maps.Map;
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+function initMap(): void {
+  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+declare global {
+  interface Window {
+    initMap: () => void;
+  }
+}
+window.initMap = initMap;
