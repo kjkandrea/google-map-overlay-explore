76c448851e5d3c950826aea4f3bbfd1809502932
kjkandrea <kjkandrea@gmail.com>
2023-03-06 13:35:23 +0900

    feat: 좌표 Sandy Hook Elementary School 로 변경

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ let map: google.maps.Map;
 
 function initMap(): void {
   map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-    center: { lat: 37.4001747, lng: 127.1034676 },
+    center: { lat: 41.419961, lng: -73.2773997 },
     zoom: 19,
     mapTypeId: 'satellite',
     disableDefaultUI: true,
