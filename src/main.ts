daa450fd7e2453da9818dbf247f088717da63126
kjkandrea <kjkandrea@gmail.com>
2023-03-06 15:35:32 +0900

    feat: 임의의 폴리곤 draw

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,34 @@
 import './style.css'
 
-// @ts-ignore
-let map: google.maps.Map;
+const sandyHookElementarySchoolLatLng : google.maps.LatLngLiteral = { lat: 41.419961, lng: -73.2773997 }
 
 function initMap(): void {
-  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-    center: { lat: 41.419961, lng: -73.2773997 },
+  const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+    center: sandyHookElementarySchoolLatLng,
     zoom: 19,
     mapTypeId: 'satellite',
     disableDefaultUI: true,
     zoomControl: true,
   });
+
+  const gymCoords: google.maps.LatLngLiteral[] = [
+    { lat: sandyHookElementarySchoolLatLng.lat - 0.00041, lng: sandyHookElementarySchoolLatLng.lng - 0.00060 },
+    { lat: sandyHookElementarySchoolLatLng.lat - 0.00028, lng: sandyHookElementarySchoolLatLng.lng - 0.00028 },
+    { lat: sandyHookElementarySchoolLatLng.lat - 0.00045, lng: sandyHookElementarySchoolLatLng.lng - 0.00016 },
+    { lat: sandyHookElementarySchoolLatLng.lat - 0.00055, lng: sandyHookElementarySchoolLatLng.lng - 0.00050 },
+  ];
+
+  // Construct the polygon.
+  const gymPolygon = new google.maps.Polygon({
+    paths: gymCoords,
+    strokeColor: "red",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "red",
+    fillOpacity: 0.35,
+  })
+
+  gymPolygon.setMap(map);
 }
 
 declare global {
